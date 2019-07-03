1f502a3ea7e309c85bb143eaffe7fea0ec46b805
YINCHU XIA <yinchu@imac.lan>
2019-07-02 21:42:42 -0700

    update 1 easy solution

--- src/_38_count_and_say.rs
+++ src/_38_count_and_say.rs
@@ -1,54 +1,36 @@
 struct Solution {}
 
-static mut s: Option<Vec<String>> = None;
-
-fn count_and_say(x: &String) -> String {
-    let n = x.len();
-    let mut count = 0;
-    let mut say = '0';
-    let mut word = String::from("");
-    for (i, c) in x.chars().enumerate() {
-        if i == 0 {
-            say = c;
-            count = 1;
-        } else {
-            if say == c {
-                count = count + 1;
-            } else {
-                word.push_str(&count.to_string());
-                word.push_str(&say.to_string());
-                say = c;
-                count = 1;
-            }
-        }
-        if i == n - 1 {
-            word.push_str(&count.to_string());
-            word.push_str(&say.to_string());
-        }
-    }
-    word
-}
-
-unsafe fn init() -> Option<Vec<String>> {
-    let mut v: Vec<String> = vec![String::from("1")];
-    for i in 1..31 {
-        let word = count_and_say(&v[i - 1]);
-        v.push(word);
-    }
-    Some(v)
+struct Pair {
+    pub digit: char,
+    pub count: usize,
 }
 
 impl Solution {
     fn count_and_say(n: i32) -> String {
-        unsafe {
-            let i: usize = n as usize - 1;
-            if None == s {
-                s = init();
+        fn next(nums: String) -> String {
+            let mut pairs: Vec<Pair> = vec![];
+            for c in nums.chars() {
+                if let Some(p) = pairs.last_mut() {
+                    if p.digit == c {
+                        p.count = p.count + 1;
+                    } else {
+                        pairs.push(Pair { digit: c, count: 1 });
+                    }
+                } else {
+                    pairs.push(Pair { digit: c, count: 1 });
+                }
             }
-            match &s {
-                Some(v) => v[i].clone(),
-                None => String::from("xxx"),
+            let mut s = String::from("");
+            for p in pairs {
+                s.push_str(&p.count.to_string());
+                s.push_str(&p.digit.to_string());
             }
+            s
+        }
+        match n {
+            1 => String::from("1"),
+            2...30 => next(Solution::count_and_say(n - 1)),
+            _ => String::from(""),
         }
     }
 }
