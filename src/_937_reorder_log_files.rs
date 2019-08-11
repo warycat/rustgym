58fbcba2702052f9700dcfe9dd6ed3bad475ddfa
YINCHU XIA <larry.fantasy@gmail.com>
2019-08-10 17:46:59 -0700

    update 1 easy solution

--- src/_937_reorder_log_files.rs
+++ src/_937_reorder_log_files.rs
@@ -3,35 +3,29 @@ struct Solution;
 use std::cmp::Ordering::*;
 
 impl Solution {
-    pub fn reorder_log_files(logs: Vec<String>) -> Vec<String> {
-        let mut digits: Vec<String> = vec![];
-        let mut letters: Vec<String> = vec![];
-        for log in logs {
-            let c: char = log.chars().last().unwrap();
-            if char::is_digit(c, 10) {
-                digits.push(log);
-            } else {
-                letters.push(log);
-            }
-        }
-
-        letters.sort_by(|a, b| {
+    pub fn reorder_log_files(mut logs: Vec<String>) -> Vec<String> {
+        logs.sort_by(|a, b| {
             let i = a.find(' ').unwrap();
             let j = b.find(' ').unwrap();
             let ar = &a[i + 1..];
             let br = &b[j + 1..];
-            let ordering = ar.cmp(br);
-            if ordering == Equal {
-                a.cmp(b)
-            } else {
-                ordering
+            let ac = a.chars().last().unwrap();
+            let bc = b.chars().last().unwrap();
+            match (char::is_digit(ac, 10), char::is_digit(bc, 10)) {
+                (true, true) => Equal,
+                (true, false) => Greater,
+                (false, true) => Less,
+                (false, false) => {
+                    let ordering = ar.cmp(br);
+                    if ordering == Equal {
+                        a.cmp(b)
+                    } else {
+                        ordering
+                    }
+                }
             }
         });
-        let mut res: Vec<String> = vec![];
-        println!("{:?} {:?}", letters, digits);
-        res.append(&mut letters);
-        res.append(&mut digits);
-        res
+        logs
     }
 }
 
