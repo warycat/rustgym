e2e6543924b8d23f2dfdc6fe84286d498f344311
Yinchu Xia <larry.fantasy@gmail.com>
2020-11-14 21:57:05 -0800

    fix tle

--- rustgym/src/leetcode/_1648_sell_diminishing_valued_colored_balls.rs
+++ rustgym/src/leetcode/_1648_sell_diminishing_valued_colored_balls.rs
@@ -9,8 +9,8 @@ impl Solution {
         let mut res: i64 = 0;
         for i in (0..n).rev() {
             let diff = if i > 0 {
-                inventory[i] - inventory[i-1]
-            }else {
+                inventory[i] - inventory[i - 1]
+            } else {
                 inventory[i]
             };
             let w = (n - i) as i32;
@@ -19,17 +19,15 @@ impl Solution {
                 res %= MOD;
                 orders -= diff * w;
             } else {
-                let mut h = inventory[i];
-                while orders >= w {
-                    res += h as i64 * w as i64;
-                    res %= MOD;
-                    orders -= w;
-                    h-=1;
-                }
+                let diff = orders / w;
+                res += (inventory[i] - diff + 1 + inventory[i]) as i64 * diff as i64 / 2 * w as i64;
+                res %= MOD;
+                orders -= diff * w;
+                let h = inventory[i] - diff;
                 while orders > 0 {
                     res += h as i64;
                     res %= MOD;
-                    orders -=1;
+                    orders -= 1;
                 }
                 break;
             }
@@ -40,19 +38,19 @@ impl Solution {
 
 #[test]
 fn test() {
-    let inventory = vec![2,5];
+    let inventory = vec![2, 5];
     let orders = 4;
     let res = 14;
     assert_eq!(Solution::max_profit(inventory, orders), res);
-    let inventory = vec![3,5];
+    let inventory = vec![3, 5];
     let orders = 6;
     let res = 19;
     assert_eq!(Solution::max_profit(inventory, orders), res);
-    let inventory = vec![2,8,4,10,6];
+    let inventory = vec![2, 8, 4, 10, 6];
     let orders = 20;
     let res = 110;
     assert_eq!(Solution::max_profit(inventory, orders), res);
-    let inventory = vec![2,8,4,10,6];
+    let inventory = vec![2, 8, 4, 10, 6];
     let orders = 20;
     let res = 110;
     assert_eq!(Solution::max_profit(inventory, orders), res);
