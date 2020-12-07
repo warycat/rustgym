4f407772a1c0f0ef99bfdcc6cbcd030792811b32
Larry Fantasy <larry.fantasy@gmail.com>
2020-12-07 14:04:49 -0800

    add ignore expensive test

--- util/src/advent_of_code.rs
+++ util/src/advent_of_code.rs
@@ -12,3 +12,19 @@ macro_rules! advent_of_code {
         }
     };
 }
+
+#[macro_export]
+macro_rules! advent_of_code_ignore {
+    ($name:ident, $input:expr, $output:expr) => {
+        #[test]
+        #[ignore]
+        fn $name() {
+            let input = include_str!($input);
+            let output = include_str!($output);
+            let mut reader = BufReader::new(input.as_bytes());
+            let mut writer = "".to_string();
+            solve(&mut reader, &mut writer);
+            assert_eq!(writer, output);
+        }
+    };
+}
