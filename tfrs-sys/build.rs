e3e474ce8c28c9f23dc3827f200ccf756c804216
Yinchu Xia <larry.fantasy@gmail.com>
2021-09-11 00:06:58 -0700

    fix cmake

--- tfrs-sys/build.rs
+++ tfrs-sys/build.rs
@@ -4,26 +4,23 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let dst: PathBuf = Config::new("../XNNPACK")
-        .build_target("XNNPACK")
+    Config::new("../XNNPACK")
+        .always_configure(false)
         .very_verbose(true)
         .build();
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-search=native={}", out_path.join("lib").display());
     println!("cargo:rustc-link-lib=static=XNNPACK");
-    println!("cargo:rustc-link-search=native={}/build/pthreadpool", dst.display());
     println!("cargo:rustc-link-lib=static=pthreadpool");
-    println!("cargo:rustc-link-search=native={}/build/cpuinfo", dst.display());
     println!("cargo:rustc-link-lib=static=cpuinfo");
-    println!("cargo:rustc-link-search=native={}/build/clog", dst.display());
     println!("cargo:rustc-link-lib=static=clog");
     let bindings = builder()
         .header("wrapper.h")
-        .clang_arg(&format!("-I/{}/include", dst.display()))
+        .clang_arg(&format!("-I/{}", out_path.join("include").display()))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .unwrap();
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
