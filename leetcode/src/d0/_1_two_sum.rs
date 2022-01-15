61bf011f70be8305e3668d751e310009ab0ab515
Srikar Tadiparthi <srikarplus@gmail.com>
2022-01-15 22:43:38 +0530

    Update _1_two_sum.rs
    use match construct

--- leetcode/src/d0/_1_two_sum.rs
+++ leetcode/src/d0/_1_two_sum.rs
@@ -16,6 +16,23 @@ impl Solution {
     }
 }
 
+// Alternate implementation
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut d: HashMap<i32, i32> = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            let c = target - num;
+            match d.get(&c) {
+                Some(&v) => {
+                    return vec![v, i as i32];
+                },
+                None => d.insert(num, i as i32)
+            };
+        }
+        vec![-1, -1]
+    }
+}
+
 #[test]
 fn test() {
     let nums = vec![2, 7, 11, 15];
