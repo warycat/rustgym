6ed01ef191589e1eb21d64dbd138a298d87cfffd
safrannn <airfluting@gmail.com>
2022-02-04 23:05:17 -0800

    fixes

--- leetcode/src/d21/_2138_divide_a_string_into_groups_of_size_k.rs
+++ leetcode/src/d21/_2138_divide_a_string_into_groups_of_size_k.rs
@@ -3,39 +3,26 @@ struct Solution;
 impl Solution {
     pub fn divide_string(s: String, k: i32, fill: char) -> Vec<String> {
         let k = k as usize;
-        let mut answer = s
-            .chars()
-            .collect::<Vec<char>>()
+        let mut answer = s.chars().collect::<Vec<char>>();
+        answer.resize(s.len() + (k - s.len() % k) % k, fill);
+        answer
             .chunks(k as usize)
             .map(|c| c.iter().collect::<String>())
-            .collect::<Vec<String>>();
-        if s.len() % k != 0 {
-            answer[s.len() / k]
-                .push_str(&vec![fill; k - s.len() % k].into_iter().collect::<String>());
-        }
-        return answer;
+            .collect::<Vec<String>>()
     }
 }
 
 #[test]
-fn test_1() {
+fn test() {
     let s = "abcdefghi".to_string();
     let k = 3;
     let fill = 'x';
-    let res = vec!["abc".to_string(), "def".to_string(), "ghi".to_string()];
+    let res = vec_string!["abc", "def", "ghi"];
     assert_eq!(Solution::divide_string(s, k, fill), res);
-}
 
-#[test]
-fn test_2() {
     let s = "abcdefghij".to_string();
     let k = 3;
     let fill = 'x';
-    let res = vec![
-        "abc".to_string(),
-        "def".to_string(),
-        "ghi".to_string(),
-        "jxx".to_string(),
-    ];
+    let res = vec_string!["abc", "def", "ghi", "jxx",];
     assert_eq!(Solution::divide_string(s, k, fill), res);
 }
