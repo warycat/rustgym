1a3e56d9880a55bcaabe18faef4a42806de290a3
m_khandaker <m_khandaker@eaglys.co.jp>
2022-12-08 18:01:49 +0900

    Box for recursive data type.

--- concept/src/bin/data_structure.rs
+++ concept/src/bin/data_structure.rs
@@ -2,17 +2,28 @@ trait Vehicle {
     fn drive(&self);
 }
 
-struct Truck;
+struct Truck {
+    name: String,
+    next_truck:Option<Box<Truck>>,
+}
 
 
 impl Vehicle for Truck {
     fn drive(&self) {
-        println!("The truck is Driving");
+       //print the next truck
+         println!("Driving {}", self.name);
+            match &self.next_truck {
+                Some(truck) => truck.drive(),
+                None => println!("No more trucks"),
+            }
     }
 }
 
 fn main(){
     let t: Box<dyn Vehicle>;
-    t = Box::new(Truck);
+    // create a list of Trucks
+    let truc2 = Truck {name: "Truck2".to_string(), next_truck: None};
+    let truc1 = Truck {name: "Truck1".to_string(), next_truck: Some(Box::new(truc2))};
+    t = Box::new(truc1);
     t.drive();
 }
